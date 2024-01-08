b51b8047162fe44d04458a5fa542c596adf0e1ed
Osamu Koga (osa_k) <github@osak.jp>
2024-01-08 23:35:28 +0900

    Add wezterm.lua

---
+++ wezterm.lua
@@ -0,0 +1,27 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = 'Catppuccin Latte'
+
+config.default_domain = 'WSL:Ubuntu'
+config.font = wezterm.font_with_fallback {
+  'Hack Nerd Font',
+  'Source Han Code JP'
+}
+config.font_size = 11
+
+-- and finally, return the configuration to wezterm
+return config
+
