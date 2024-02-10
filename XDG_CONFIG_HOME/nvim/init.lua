4e3d4ab28d6ca9d075d72d079824dc2f2ceb4dc5
Osamu Koga <github@osak.jp>
2024-02-11 00:55:44 +0900

    Don't let cmp-cmdline complete broken paths

--- XDG_CONFIG_HOME/nvim/init.lua
+++ XDG_CONFIG_HOME/nvim/init.lua
@@ -44,7 +44,7 @@ cmp.setup({
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
-    { name = 'vsnip' }, -- For vsnip users.
+    -- { name = 'vsnip' }, -- For vsnip users.
     -- { name = 'luasnip' }, -- For luasnip users.
     -- { name = 'ultisnips' }, -- For ultisnips users.
     -- { name = 'snippy' }, -- For snippy users.
@@ -76,7 +76,10 @@ cmp.setup.cmdline(':', {
   sources = cmp.config.sources({
     { name = 'path' }
   }, {
-    { name = 'cmdline' }
+    {
+      name = 'cmdline',
+      option = { ignore_cmds = {"Man", "!", "edit", "write", } }
+    }
   })
 })
 
